55c2cc1e1bb793bfcedba9efdf9b8bb27279e5f2
Govind Radhakrishnan Nair <82027436+govindr15@users.noreply.github.com>
2022-02-10 03:01:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -193,8 +193,7 @@ app.use("/feedback", (req, res, next) => {
 
 configRoutes(app);
 
-app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
